edeb1fd4a6f9fbaa16b55eeef4de8778b409e211
Garau <nicholas.garau@accenture.com>
2022-05-02 11:41:59 +0200

    updating class parser with answer parser

--- scraper_4finetuning.py
+++ scraper_4finetuning.py
@@ -1,11 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-from urllib.parse import urljoin, urlparse
 
 
 class Parser:
-    '''A class with different method to parse a webpage''' #todo:completa docstring con l'elenco dei metodi di classe
+    """
+    A class to parse an html page.
+
+    Attributes
+    ------------
+        url=the page's url to scrape.
+
+    Methods
+    ------------
+    html_parser():
+        a method that saves parsed html on the 'rawtext' slot.
+
+    questions_parser():
+        a method to filter the questions of the page and saves them on the 'questions' slot.
+
+    question_answers():
+        a method to filter the answers of the page and saves them on the 'answers' slot.
+
+
+
+    """
 
     def __init__(self, url=str):
         self.url = url
@@ -22,22 +41,27 @@ class Parser:
 
     def questions_parser(self):
         """a method that return questions"""
+        self.html_parser()
         pattern_questions = '<p><strong>'
-        self.questions = [str(item).replace('<p><strong>', '').replace('</strong></p>', '') for item in self.rawtext
+        self.questions = [str(item).replace('<p><strong>', '').replace('</strong></p>', '')[4:] for item in self.rawtext
                           if pattern_questions in str(item)]
         return self.questions
 
     def answers_parser(self):
         """a method that return answers"""
-        pattern_questions = '<p><strong>'
-        self.questions = [str(item).replace('<p>', '').replace('</p>', '') for item in self.rawtext
-                          if pattern_questions not in str(item)]
-        return self.questions  #todo: completa questo metodo
+        self.html_parser()
+        pattern_answers = '<p>R'
+        self.answers = [str(item).replace('<p>', '').replace('</p>', '')[4:] for item in self.rawtext
+                        if pattern_answers in str(item)]
+        lost_sentence = ''.join(str(self.rawtext[3:6])).replace('<p>', '').replace('</p>', '').replace('.,', '.')
+        self.answers[0] = self.answers[0] + lost_sentence.replace('[', '').replace(']', '')
+        return self.answers
+
+    def __call__(self, *args, **kwargs):
+        return f'Questions: \n {self.questions_parser()}\n\n Answers: \n {self.answers_parser()} '
 
 
 if __name__ == '__main__':
     url = 'https://www.agid.gov.it/it/node/1638'
     parser = Parser(url)
-    print(parser.html_parser()[:10])
-    #print(parser.questions_parser())
-    print(parser.answers_parser())
+    print(parser())
