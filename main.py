a3ca4c2110c10e56b1c2c3b644822b432a5a1513
Garau <nicholas.garau@accenture.com>
2022-05-03 12:01:11 +0200

    updating main and scraping file - adding jsonl file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Main del progetto')
+import scraper_4finetuning
+
+'''Scraping html SPID-faq page for the fine tuning and generating jsonl file'''
+
+url = 'https://www.agid.gov.it/it/node/1638'
+parser = scraper_4finetuning.Parser(url)
+questions = parser.questions_parser()
+answers = parser.answers_parser()
+# print(questions[14]) #not a question
+questions.remove(questions[14])
+ds = scraper_4finetuning.DatasetGenerator(questions, answers)
+ds.create_jsonl()
--- scraper_4finetuning.py
+++ scraper_4finetuning.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import jsonlines
+import numpy as np
 
 
 class Parser:
@@ -53,7 +54,7 @@ class Parser:
         pattern_answers = '<p>R'
         self.answers = [str(item).replace('<p>', '').replace('</p>', '')[4:] for item in self.rawtext
                         if pattern_answers in str(item)]
-        lost_sentence = ''.join(str(self.rawtext[3:6])).replace('<p>', '').replace('</p>', '').replace('.,', '.')
+        lost_sentence = ''.join(str(self.rawtext[3:8])).replace('<p>', '').replace('</p>', '').replace('.,', '.')
         self.answers[0] = self.answers[0] + lost_sentence.replace('[', '').replace(']', '')
         return self.answers
 
@@ -71,15 +72,20 @@ class DatasetGenerator:
 
     def create_jsonl(self):
         """a method that generate a jsonl file"""
-        items = [{"prompt": item1, "completion": item2} for item1, item2 in self.col1 + self.col2]
-        return items
-        #todo: prova a creare un array e fare uno step precedente alla creazione del jsonl, cosi passi al metodo il csv o un pandas
-
+        items = [{"prompt": item1, "completion": item2} for item1, item2 in np.column_stack((self.col1, self.col2))]
+        with jsonlines.open('faq_finetuning.jsonl', 'w') as writer:
+            writer.write_all(items)
+        return None
 
 if __name__ == '__main__':
     url = 'https://www.agid.gov.it/it/node/1638'
     parser = Parser(url)
     questions = parser.questions_parser()
     answers = parser.answers_parser()
+    #print(questions[14]) #not a question
+    questions.remove(questions[14])
+
+
     ds = DatasetGenerator(questions, answers)
-    print(ds.create_jsonl())
+    ds.create_jsonl()
+
