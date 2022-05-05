783754fe3c781fa156edb010aeae20e4dff64c45
Garau <nicholas.garau@accenture.com>
2022-05-05 13:00:22 +0200

    updating chatbot skeleton

--- chatbot_skeleton.py
+++ chatbot_skeleton.py
@@ -2,28 +2,31 @@ import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+completion = openai.Completion()
 
 start_sequence = "\nSimone:"
 restart_sequence = "\nTu: "
-
-response = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt="Ciao, benvenuto nel servizio di assistenza di AGID, l'assistente è Simone e sarà in grado di fornirti assistenza."
-         "\n\nTu: Ciao Simone"
-         "\nSimone: Ciao, sono Simone, come posso aiutarti?"
-         "\nTu: Tu lavori per lo Stato? "
-         "\n\nSimone: Si, sono un assistente sviluppato da AGID per dare assistenza al cittadino"
-         "\nTu: Mi servirebbero informazioni sullo SPID"
-        "\nSimone:"
-         "\nTu:",
-  temperature=0.50,
-  max_tokens=250,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0.6,
-  stop=[" Tu:", " Simone:"]
-
-)
+session_prompt = "Ciao, benvenuto nel servizio di assistenza di AGID, l'assistente è Simone e sarà in grado di fornirti assistenza.\n\nTu: Ciao Simone \nSimone: Ciao, sono Simone, come posso aiutarti?"
 
 
-print(response)
+def ask(question, chat_log=None):
+    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt_text,
+        temperature=0.50,
+        max_tokens=250,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Tu:", " Simone:"]
+    )
+    story = response['choices'][0]['text']
+    return str(story)
+
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    """a function that help the chatbot to remember"""
+    if chat_log is None:
+        chat_log = session_prompt
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
