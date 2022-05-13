2751801dbb4525e62553d396d0a776a39260c565
Garau <nicholas.garau@accenture.com>
2022-05-13 13:10:51 +0200

    update chat_history on chatbot

--- app_chatbot.py
+++ app_chatbot.py
@@ -1,23 +1,38 @@
-import json
-
-from flask import Flask, request, session, jsonify
-from flask_cors import CORS
 from dotenv import load_dotenv
-from chatbot_skeleton import chat
+from flask import Flask, request, jsonify, session
+from flask_cors import CORS
+
+from chatbot_skeleton import chat, create_history
 
 app = Flask(__name__)
 load_dotenv()
 CORS(app)
+
+chat_history = []
+model = 'text-davinci-002'
 
 
 @app.route("/chat", methods=['POST'])
 def bot():
     user_input = request.json
     response_json = {"reply": ""}
-    answer = chat(user_input, chat_log=None)
+    global model, chat_history
+    answer = chat(user_input, model, chat_history)
+    chat_history.append(create_history(user_input, answer, chat_history)) #todo: valuta se farlo con il +=, gira ma devi correggere il prompt
+    print(chat_history)
     response_json["reply"] = answer
     response_json = jsonify(response_json)
     return response_json
+
+
+# @app.route("/load_model", methods=["POST"])
+# def load_model():
+#   user_input = request.json
+#  response_json = {"reply": ""}
+# answer = chat(user_input, model, chat_log=None)
+# response_json["reply"] = answer
+# response_json = jsonify(response_json)
+# return response_json
 
 
 if __name__ == '__main__':
--- chatbot_skeleton.py
+++ chatbot_skeleton.py
@@ -1,34 +1,36 @@
 import os
 import openai
 
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.Completion()
 
 start_sequence = "\nSimone:"
-restart_sequence = "\nTu: "
+restart_sequence = "\n\nTu: "
 session_prompt = "Ciao, benvenuto nel servizio di assistenza di AGID, l'assistente è Simone e sarà in grado di fornirti assistenza.\n\nTu: Ciao Simone \nSimone: Ciao, sono Simone, come posso aiutarti?"
 
 
-def chat(request_json, chat_log=None):
+def chat(request_json, model=str, chat_log=None):
     """The main function which generate the response from gpt3 Q&A model"""
     question = request_json['user_input']
     if (chat_log == None):
         chat_log = session_prompt
     prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
     response = completion.create(
-        engine="text-curie-001",
+        engine=model,
         prompt=prompt_text,
         temperature=0.60,
         max_tokens=250,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
-        stop=[" Tu:", " Simone:"]
+        stop=["\n"]
     )
     reply = response['choices'][0]['text']
     return str(reply)
 
 
-
-
+def create_history(question, answer, chat_log=None):
+    """A function to create an history for chatbot"""
+    if chat_log is None:
+        chat_log = session_prompt
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
