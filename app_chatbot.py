b0910031dd37b79fa14757904f676b797a46d70e
Garau <nicholas.garau@accenture.com>
2022-05-17 11:58:36 +0200

    updating skeleton and app with loggers

--- app_chatbot.py
+++ app_chatbot.py
@@ -1,28 +1,36 @@
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import logging
 from chatbot_skeleton import chat
 
 app = Flask(__name__)
 load_dotenv()
 CORS(app)
 
-chat_history = []
+"""logger setup and model instantiation"""
+logger = logging.getLogger()
+chat_history = None
+model = "text-davinci-002"
 
 
 @app.route("/chat_GPT3_davinci", methods=['POST'])
 def bot():
     user_input = request.json
     response_json = {"reply": ""}
-    global chat_history
-#    if user_input["user_input"] == "clean history":
-#        chat_history = []
-    answer, chat_history = chat(user_input, chat_history)
-    print(chat_history)
-    response_json["reply"] = answer
-    response_json = jsonify(response_json)
-    return response_json
+    try:
+        global chat_history, model
+        if user_input["user_input"] == "clean history":
+            chat_history = None
+        answer, chat_history = chat(user_input, model, chat_history)
+        print(chat_history)
+        response_json["reply"] = answer
+        response_json = jsonify(response_json)
+        return response_json
+    except Exception as e:
+        response_json = jsonify({'error': 'app dialog service error'})
+        logger.info(" APP DIALOG ERROR", exc_info=e)
+        return response_json
 
 
 # @app.route("/load_model", methods=["POST"])
--- chatbot_skeleton.py
+++ chatbot_skeleton.py
@@ -1,9 +1,15 @@
 import os
 import openai
+import logging
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.Completion()
 
+"""logger setup"""
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+"""Prompt definition"""
 start_sequence = "\nSimone:"
 restart_sequence = "\nTu: "
 session_prompt = """
@@ -13,24 +19,31 @@ Simone: Ciao, sono Simone, come posso aiutarti?
 """
 
 
-def chat(request_json, chat_log=None):
-    """The main function which generate the response from gpt3 Q&A model"""
+def chat(request_json, model=str, chat_log=None):
+    """
+    The main function which generate the response from gpt3 Q&A model
+    """
     question = request_json['user_input']
-    if chat_log is None:
-        chat_log = session_prompt
-    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
-    response = completion.create(
-        engine="text-davinci-002",
-        prompt=prompt_text,
-        temperature=0.80,
-        max_tokens=250,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\nTu:"]
-    )
-    reply = response['choices'][0]['text']
-    if chat_log is None:
-        chat_log = session_prompt
-    chat_log += f"\nTu: {question}\nSimone: {reply}"
-    return str(reply), ''.join(chat_log)
+    try:
+        logger.info(f' GPT3 user input: {question}')
+        if chat_log is None:
+            chat_log = session_prompt
+        prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+        response = completion.create(
+            engine=model,
+            prompt=prompt_text,
+            temperature=0.80,
+            max_tokens=250,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6,
+            stop=["\nTu:"]
+        )
+        reply = response['choices'][0]['text']
+        #if chat_log is None:
+        #    chat_log = session_prompt
+        chat_log += f"\nTu: {question}\nSimone: {reply}"
+        return str(reply), ''.join(chat_log)
+
+    except RuntimeError:
+        logger.error("ERROR in Dialog GPT3 pipeline", exc_info=True)
