dfa0634687b200b0bd90b1b28a8d05f53dec211f
Garau <nicholas.garau@accenture.com>
2022-05-24 11:14:55 +0200

    loading q&a model first implementation

--- app_chatbot.py
+++ app_chatbot.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import logging
 from chatbot_skeleton import chat
+from q_and_a_model import question_answering
 
 app = Flask(__name__)
 load_dotenv()
@@ -33,14 +34,21 @@ def bot():
         return response_json
 
 
-# @app.route("/load_model", methods=["POST"])
-# def load_model():
-#   user_input = request.json
-#  response_json = {"reply": ""}
-# answer = chat(user_input, model, chat_log=None)
-# response_json["reply"] = answer
-# response_json = jsonify(response_json)
-# return response_json
+@app.route("/q&a_solution", methods=["POST"])
+def q_and_a():
+    user_input = request.json
+    response_json = {"reply": ""}
+    try:
+        global chat_history
+        answer, chat_history = question_answering(user_input, model, chat_history)
+        response_json["reply"] = answer
+        response_json = jsonify(response_json)
+        return response_json
+    except Exception as e:
+        response_json = jsonify({'error': 'app dialog service error'})
+        logger.info(" APP DIALOG ERROR", exc_info=e)
+        return response_json
+
 
 
 if __name__ == '__main__':
---
+++ q_and_a_model.py
@@ -0,0 +1,33 @@
+import os
+import openai
+import logging
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+file = openai.File.create(file=open("faq_answers.jsonl", encoding="utf8"), purpose='answers')
+
+"""logger setup"""
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+
+def question_answering(request_json, model=str, chat_hist=None):
+    question = request_json['user_input']
+    try:
+        response = openai.Answer.create(
+            search_model="ada",
+            model=model,
+            question=question,
+            file=file["id"],
+            examples_context="Lo Stato italiano ha deciso di stanziare fondi per la digitalizzazione del paese",
+            examples=[["A quanto ammonta il finanziamento che lo Stato italiano ha previsto per la digitalizzazione?",
+                       "49,86 miliardi di euro."]],
+            max_rerank=10,
+            max_tokens=5,
+            stop=["\n", "<|endoftext|>"]
+        )
+        print(response)
+        reply = response[0]
+        chat_hist += f"\nTu: {question}\nSimone: {reply}"
+        return str(reply), ''.join(chat_hist)
+    except RuntimeError:
+        logger.error("ERROR in Dialog GPT3 pipeline", exc_info=True)
