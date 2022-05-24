7d8185cf00079c94f4bacd15f3ee0f3c27474b19
Garau <nicholas.garau@accenture.com>
2022-05-24 12:45:17 +0200

    updating main file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import scraper_4finetuning
+import scraper
 """
 '''Scraping html SPID-faq page for the fine tuning and generating jsonl file'''
 
@@ -12,4 +12,23 @@ ds = scraper_4finetuning.DatasetGenerator(questions, answers)
 ds.create_jsonl()
 
 """
+
+"""
+Scraping html SPID-PEC-SIOPE main page
+
+urlspid = "https://www.agid.gov.it/it/piattaforme/spid"
+urlpec = "https://www.agid.gov.it/it/piattaforme/posta-elettronica-certificata"
+urlsiope = "https://www.agid.gov.it/it/piattaforme/siope"
+parser_spid = Parser(urlspid).html_parser()
+parser_pec = Parser(urlpec).html_parser()
+parser_siope = Parser(urlsiope).html_parser()
+
+corpus_dict = [{"text": parser_spid.strip(), "metadata": "documentazione spid"},
+                {"text": parser_pec.strip(), "metadata": "documentazione pec"},
+                {"text": parser_siope.strip(), "metadata": "documentazione siope"}
+                ]
+with jsonlines.open('json_files/corpus_agid.jsonl', 'w') as writer:
+    writer.write_all(corpus_dict)
+"""
+
 
