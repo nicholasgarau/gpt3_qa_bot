0e3c4bca4a9f62815c711a0f37a80311029fe996
Garau <nicholas.garau@accenture.com>
2022-06-09 12:27:51 +0200

    dataframe integration in q&a model

--- q_and_a_model.py
+++ q_and_a_model.py
@@ -2,23 +2,21 @@ import os
 import openai
 import logging
 
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 """logger setup"""
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
 
-def question_answering(request_json, model=str):
+def question_answering(request_json, faqs_list, model=str):
     question = request_json['user_input']
     try:
         response = openai.Answer.create(
             search_model="ada",
             model=model,
             question=question,
-            file=os.getenv("FILE_SPID_BASIC"),
+            documents=faqs_list,
             examples_context="La PEC è la Posta Elettronica Certificata",
             examples=[["Cosa significa PEC?",
                        "Posta Elettronica Certificata"]],
@@ -26,7 +24,7 @@ def question_answering(request_json, model=str):
             max_tokens=70,
             stop=["\n", "<|endoftext|>"]
         )
-        #print(response)
+        # print(response)
         if response["answers"][0] == "":
             reply = "Gentile utente, la prego di riformulare la domanda"
         else:
@@ -34,5 +32,3 @@ def question_answering(request_json, model=str):
         return str(reply)
     except RuntimeError:
         logger.error("ERROR in Dialog Q&A pipeline", exc_info=True)
-
-
